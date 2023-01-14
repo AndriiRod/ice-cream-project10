a661350ea85b382c98d6c6168bbdc4e139a4a7d7
AndriiRod <115430548+AndriiRod@users.noreply.github.com>
2023-01-14 04:17:07 +0000

    Deploying to gh-pages from @ AndriiRod/ice-cream-project10@bc06ebbeded7f393318ed51ab3bfd6bff036d9f2 🚀

--- index.74984fdf.js
+++
@@ -1,2 +0,0 @@
-$(".custom-reviews__list").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
-//# sourceMappingURL=index.74984fdf.js.map
--- index.f47cb0e9.js
+++
@@ -1,2 +0,0 @@
-$(".custom-reviews__list").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
-//# sourceMappingURL=index.f47cb0e9.js.map
